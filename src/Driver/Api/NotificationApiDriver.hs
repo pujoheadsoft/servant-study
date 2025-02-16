3a8e958688b65d05e31942cf7c7b86849e0258e8
Pujo <pujoheadsoft@gmail.com>
2025-02-17 07:31:41 +0900

    wip

--- src/Driver/Api/NotificationApiDriver.hs
+++ src/Driver/Api/NotificationApiDriver.hs
@@ -7,6 +7,8 @@ import Text.URI (mkURI)
 import Data.Text (pack, Text)
 import Control.Exception.Safe (throwIO, Exception)
 import Control.Monad.IO.Class (MonadIO)
+import Data.Aeson.Text (encodeToLazyText)
+import Data.Text.Lazy.Encoding (encodeUtf8)
 
 post
   :: (ToJSON payload, FromJSON response, MonadIO m)
@@ -18,15 +20,18 @@ post settings endpoint payload = runReq defaultHttpConfig $ do
   uri <- mkURI $ pack settings.baseUrl
   url <- maybe (throwIO (ParseURIException $ "Error: URI not found: " <> settings.baseUrl)) pure (useURI uri)
   response <- either toRequest toRequest url
-
   pure (responseBody response)
   where
     toRequest :: FromJSON b => (Url scheme, Option scheme) -> Req (JsonResponse b)
-    toRequest (u, o) = req POST (u /: "notifications" /: endpoint) (ReqBodyJson payload) jsonResponse o
+    toRequest (u, o) = do
+      let lbs = encodeUtf8 $ encodeToLazyText payload
+      req POST (u /: "notifications" /: endpoint) (ReqBodyLbs lbs) jsonResponse o
 
 postMessage :: MonadIO m => NotificationApiSettings -> Text -> m ()
 postMessage settings message = do
-  let payload = object [ "message" .= message ]
+  let
+    text = "ユーザーID: 1で、名無しの権兵衛さんが登録されました。" :: Text
+    payload = object [ "message" .= text ]
   post settings (pack "userRegistered") payload
 
 newtype ParseURIException = ParseURIException String

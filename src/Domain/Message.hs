df3be4fa02778e2c4af147e44b76de34ceee80b2
Pujo <pujoheadsoft@gmail.com>
2025-02-27 20:04:56 +0900

    wip

--- src/Domain/Message.hs
+++ src/Domain/Message.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Domain.Message (Message, registeredUserMessage, HasValue(..)) where
 
 import Control.Lens (makeFieldsId)
-import Data.Text (Text, pack)
+import Data.Text (Text)
+import Data.String.Interpolate (i)
 import Domain.User
 
 newtype Message = Message { value :: Text }
@@ -13,4 +15,4 @@ makeFieldsId ''Message
 registeredUserMessage :: UserId -> UserName -> Message
 registeredUserMessage userId userName = do
   let n = userName.last <> userName.first
-  Message $ pack "ユーザーID: " <> pack (show userId) <> pack "で、" <> n <> pack "さんが登録されました。"
+  Message [i|ユーザーID: #{userId}で、#{n}さんが登録されました。|]

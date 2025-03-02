b281d7cc1f2acb94c3b8bc704b56a47c2c537307
Pujo <pujoheadsoft@gmail.com>
2025-03-03 07:38:57 +0900

    wip

---
+++ src/Architecture/Heftia/Common/Logger.hs
@@ -0,0 +1,17 @@
+module Architecture.Hefty.Common.Logger where
+
+import Control.Monad.Hefty (makeEffectF, type (:!!), type (~>), type (<|), interpret)
+import Control.Monad.Logger (runStdoutLoggingT, logErrorN, logWarnN, logInfoN)
+import Data.Text (pack)
+
+data Logger a where
+  LogInfo :: Show a => a -> Logger ()
+  LogWarn :: Show a => a -> Logger ()
+  LogError :: Show a => a -> Logger ()
+makeEffectF [ ''Logger ]
+
+runLogger :: IO <| ef => eh :!! Logger ': ef ~> eh :!! ef
+runLogger = interpret \case
+  LogInfo a -> runStdoutLoggingT $ logInfoN $ pack $ show a
+  LogWarn a -> runStdoutLoggingT $ logWarnN $ pack $ show a
+  LogError a -> runStdoutLoggingT $ logErrorN $ pack $ show a
